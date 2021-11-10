e7928e56a080c7e22d4d8e8b898c27c7438b4c31
zombie1864 <zombie1864@gmail.com>
2021-11-10 11:05:25 -0500

    added doc str to ToDoListTemplate

--- src/templates/ToDoListTemplate.js
+++ src/templates/ToDoListTemplate.js
@@ -16,8 +16,19 @@ const ToDoListTemplate = (
 ) => {
     return (
         /**
-        @description: 
-        @param {} 
+        @description: view function, renders to do list 
+        @param {toDoList - state} Array: contains state of to do items, list of strings 
+        @param {input - state} String: input from user 
+        @param {toggleNewItem - state} Boolean: flag/switch to toggle between new item state 
+        @param {itemToEditIndex - state} String: the idx of item in toDoList 
+        @param {searchTerm - state} String: search term input from user 
+        @param {addNewItem - event handler} cdFunc: deals with adding new items to toDoList state 
+        @param {handleInputChange - event handler} cdFunc: handles input changes 
+        @param {saveNewItem - event handler} cdFunc: deals with saving new item to toDoList state 
+        @param {editItem - event handler} cdFunc: deals with editing item from toDoList state 
+        @param {deleteItem - event handler} cdFunc: deals with deleteing item form toDoList state 
+        @param {handleSearchRequest - event handler} cdFunc: deals with handling search term 
+        @param {cancelAddNewItem - event handler} cdFunc: cancels the attempt to add new item 
         **/
         <div className='toDoListContainer'>
         <div className='toDoListTopMenu'>
