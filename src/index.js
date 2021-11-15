b800d5b72e6a90b065a0085883cf8e598b4144ed
zombie1864 <zombie1864@gmail.com>
2021-11-15 11:50:29 -0500

    minor change to index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import BrowserRouter from 'react-router-dom/BrowserRouter'
 
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
